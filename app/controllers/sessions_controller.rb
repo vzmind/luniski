3c48a74d04a95f73e43f573583791ee9eef633d5
Vzmind <vzmind@gmail.com>
2011-11-04 23:40:10 +0100

    Successful connexion to SFDC

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
   def create
+    # Storing token
     ENV['sfdc_token'] = request.env['omniauth.auth']['credentials']['token']
     ENV['sfdc_instance_url'] = request.env['omniauth.auth']['instance_url']
+
+    # Create connexion to SFDC based on received token
+    @client = Databasedotcom::Client.new("config/databasedotcom.yml")
+    @client.authenticate :token => ENV['sfdc_token'], :instance_url => ENV['sfdc_instance_url'] 
+
+    #debug output
     render :text => request.env['omniauth.auth'].inspect
   end
 
