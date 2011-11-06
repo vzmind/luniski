0381b19620aaf79d2ae4c046ba7438536e123cd0
Vzmind <vzmind@gmail.com>
2011-11-06 15:39:29 +0100

    before filter to force login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #before_filter :check_sfdc, :except => [:login]
+  before_filter :check_user, :except => [:login]
   
-  def check_sfdc
-    if ENV['sfdc_token'].nil?
+  def check_user
+    if session[:token].nil?
       redirect_to "/sessions/login"
     end
   end
