9657c59a25467dbb4d2cc1dd57bc0cf0c0a211ce
Vzmind <vzmind@gmail.com>
2011-11-13 18:22:29 +0100

    my cases box content + simple feeds content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 
 class HomeController < ApplicationController
   def index
+    @sfdc = Databasedotcom::Client.new("config/databasedotcom.yml")
+    @sfdc.authenticate :token => session[:token], :instance_url => ENV['sfdc_instance_url']
+    @sfdc.materialize("Case")
+
+    @cases = Case.find_all_by_OwnerId(session[:user_id])
   end
 end
