fb17182c76d12a943d9dae58f38f91224f0663f2
Vzmind <vzmind@gmail.com>
2011-11-13 17:15:46 +0100

    Add drag and drop on home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+// Custom Javascript
+
+$(document).ready(function() {
+	
+  $('.dragbox').each(function(){
+    $(this).hover(function(){
+      $(this).find('h2').addClass('collapse');
+    }, function(){
+      $(this).find('h2').removeClass('collapse');
+    })
+    .find('h2').hover(function(){
+      $(this).find('.configure').css('visibility', 'visible');
+    }, function(){
+      $(this).find('.configure').css('visibility', 'hidden');
+    })
+    .click(function(){
+      $(this).siblings('.dragbox-content').toggle();
+    })
+    .end()
+    .find('.configure').css('visibility', 'hidden');
+  });
+  $('.column').sortable({
+    connectWith: '.column',
+    handle: 'h2',
+    cursor: 'move',
+    placeholder: 'placeholder',
+    forcePlaceholderSize: true,
+    opacity: 0.4,
+    stop: function(event, ui){
+      $(ui.item).find('h2').click();
+    }
+  })
+  .disableSelection();
+});
