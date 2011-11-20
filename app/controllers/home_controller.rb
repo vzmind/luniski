a61b9276aff9406839ed66d4328222982cd64188
Vzmind <vzmind@gmail.com>
2011-11-20 22:34:11 +0100

    removing caching on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 
 class HomeController < ApplicationController
   def index
-    response.headers['Cache-Control'] = 'public, max-age=300'
+    response.headers['Cache-Control'] = 'public, max-age=0'
     @sfdc = Databasedotcom::Client.new("config/databasedotcom.yml")
     @sfdc.authenticate :token => session[:token], :instance_url => ENV['sfdc_instance_url']
     @sfdc.materialize("Case")
