96c7dd5450bfaffd9d8c757355fe2e6780edbe56
Vzmind <vzmind@gmail.com>
2011-11-21 20:34:53 +0100

    Plug EV test ORG

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,10 +17,16 @@ class SessionsController < ApplicationController
 
   def signin
     client = Savon::Client.new do
-      wsdl.document = File.expand_path(Rails.root + "config/wsdl.jsp-new-org.xml", __FILE__)
+      # Oxala ORG
+      #wsdl.document = File.expand_path(Rails.root + "config/wsdl.jsp-new-org.xml", __FILE__)
+      # EV ORG
+      wsdl.document = File.expand_path(Rails.root + "config/wsdl.jsp-ev-org.xml", __FILE__)
     end
     @response = client.request :login do
-      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DU0000000K0FF</urn:organizationId><urn:portalId>060U0000000Trku</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # Oxala ORG
+      #r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DU0000000K0FF</urn:organizationId><urn:portalId>060U0000000Trku</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # EV ORG
+      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DM00000004dOS</urn:organizationId><urn:portalId>060D0000000QE51</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
       soap.xml = r
     end
     @response = @response.to_hash
--- config/initializers/app_init.rb
+++ config/initializers/app_init.rb
@@ -1,4 +1,7 @@
 # Old Org
-ENV['sfdc_instance_url'] = "https://eu1.salesforce.com"
+#ENV['sfdc_instance_url'] = "https://eu1.salesforce.com"
 # New Org
-ENV['sfdc_instance_url'] = "https://na12.salesforce.com"
+#ENV['sfdc_instance_url'] = "https://na12.salesforce.com"
+# EV ORG
+ENV['sfdc_instance_url'] = "https://cs7.salesforce.com"
+
