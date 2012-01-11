1d614d2ed9db459ed1f9c385e4168ccc45c92882
vzmind <vzmind@gmail.com>
2012-01-11 21:54:17 +0100

    Luniski credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,8 @@ class SessionsController < ApplicationController
       #r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DU0000000K0FF</urn:organizationId><urn:portalId>060U0000000Trku</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
       # EV ORG
       r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DM00000004dOS</urn:organizationId><urn:portalId>060D0000000QE51</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # Luniski ORG
+      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00Dd0000000bt0I</urn:organizationId><urn:portalId>060d0000000PoFK</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
       soap.xml = r
     end
     @response = @response.to_hash
--- config/initializers/app_init.rb
+++ config/initializers/app_init.rb
@@ -1,7 +1,7 @@
 # Old Org
 #ENV['sfdc_instance_url'] = "https://eu1.salesforce.com"
 # New Org
-#ENV['sfdc_instance_url'] = "https://na12.salesforce.com"
+ENV['sfdc_instance_url'] = "https://na14.salesforce.com"
 # EV ORG
-ENV['sfdc_instance_url'] = "https://cs7.salesforce.com"
+#ENV['sfdc_instance_url'] = "https://cs7.salesforce.com"
 
