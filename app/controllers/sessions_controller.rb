9ddb448aaddfe7486bf27b2c43439d4b514deeec
vzmind <vzmind@gmail.com>
2012-01-11 21:54:17 +0100

    resolve conflicts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,12 @@ class SessionsController < ApplicationController
       #wsdl.document = File.expand_path(Rails.root + "config/wsdl.jsp-ev-org.xml", __FILE__)
     end
     @response = client.request :login do
-      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DU0000000K0FF</urn:organizationId><urn:portalId>060U0000000Trku</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # Oxala ORG
+      #r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DU0000000K0FF</urn:organizationId><urn:portalId>060U0000000Trku</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # EV ORG
+      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00DM00000004dOS</urn:organizationId><urn:portalId>060D0000000QE51</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
+      # Luniski ORG
+      r = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:enterprise.soap.sforce.com'><soapenv:Header><urn:LoginScopeHeader><urn:organizationId>00Dd0000000bt0I</urn:organizationId><urn:portalId>060d0000000PoFK</urn:portalId></urn:LoginScopeHeader></soapenv:Header><soapenv:Body><urn:login><urn:username>%s</urn:username><urn:password>%s</urn:password></urn:login></soapenv:Body></soapenv:Envelope>" % [params['auth']['login'],params['auth']['password']]
       soap.xml = r
     end
     @response = @response.to_hash
